40cf7f4012701d5700d9127256920fefd6e29c52
Jakub Szum <master_szumi@o2.pl>
2021-04-15 10:22:51 +0200

    First animation

--- index.php
+++ index.php
@@ -31,9 +31,7 @@ function calc_end_pos(begin_pos, len, angle) {
   return new Point(end_x, end_y);
 }
 
-function draw_line(begin_pos, end_pos, dash) {
-  var c = document.getElementById("myCanvas");
-  var ctx = c.getContext("2d");
+function draw_line(ctx, begin_pos, end_pos, dash) {
   ctx.beginPath();
   ctx.setLineDash(dash);
   ctx.moveTo(begin_pos.x, begin_pos.y);
@@ -42,19 +40,99 @@ function draw_line(begin_pos, end_pos, dash) {
   ctx.setLineDash([]);
 }
 
+function draw_player(ctx, pos, angle, color, has_ball) {
+  ctx.save();
+  var r = 10;
+  ctx.translate(pos.x, pos.y);
+  ctx.rotate(angle);
+  //feets
+  ctx.beginPath();
+  ctx.arc(0 + 3*r/4, 0 - 2*r/3, r/2, 0, 2 * Math.PI);
+  ctx.arc(0 + 3*r/4, 0 + 2*r/3, r/2, 0, 2 * Math.PI);
+  ctx.fillStyle = "#000000";
+  ctx.fill();
+  if (has_ball) {
+    // ball
+    ctx.beginPath();
+    ctx.arc(0 + 3*r/2, 0 - r, r/2, 0, 2 * Math.PI);
+    ctx.fillStyle = "#ffffff";
+    ctx.fill();
+    ctx.stroke();
+  }
+  // body
+  ctx.beginPath();
+  ctx.ellipse(0, 0, r, r + r/2, 0, 0, 2 * Math.PI)
+  ctx.fillStyle = color;
+  ctx.fill();
+  ctx.stroke();
+  // head
+  ctx.beginPath();
+  ctx.arc(0, 0, r/2, 0, 2 * Math.PI);
+  ctx.fillStyle = "#000000";
+  ctx.fill();
+  ctx.restore();
+}
+
+function draw_ball(ctx, pos) {
+  ctx.beginPath();
+  var r = 5;
+  ctx.arc(pos.x, pos.y, r, 0, 2 * Math.PI);
+  ctx.fillStyle = "#ffffff";
+  ctx.fill();
+  ctx.stroke();
+}
+
+function draw_message(ctx, message, pos, color, font) {
+  ctx.fillStyle = color;
+  ctx.font = font;
+  ctx.fillText(message, pos.x, pos.y);
+}
+
+class Animation {
+  constructor(ctx) {
+    this.ctx = ctx;
+    this.promise = new Promise((resolve, reject) => { resolve(); });
+  }
+
+  push(func, delay) {
+    this.promise = this.promise.then(() => {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          func(this.ctx);
+          resolve();
+        }, delay);
+      });
+    });
+  }
+
+  draw_line(begin_pos, end_pos, dash) {
+    this.push((ctx) => { draw_line(ctx, begin_pos, end_pos, dash) }, 300);
+  }
+
+  draw_player(pos, angle, color, has_ball) {
+    this.push((ctx) => { draw_player(ctx, pos, angle, color, has_ball) }, 0);
+  }
+
+  draw_ball(pos) {
+    this.push((ctx) => { draw_ball(ctx, pos) }, 0);
+  }
+
+  draw_message(message, pos, color, font) {
+    this.push((ctx) => { draw_message(ctx, message, pos, color, font) }, 200);
+  }
+};
+
+var animation = null;
+
 class Ball {
   constructor(ctx, pos) {
     this.ctx = ctx;
     this.pos = pos;
   }
 
-  draw(pos) {
-    this.ctx.beginPath();
-    var r = 5;
-    this.ctx.arc(this.pos.x, this.pos.y, r, 0, 2 * Math.PI);
-    this.ctx.fillStyle = "#ffffff";
-    this.ctx.fill();
-    this.ctx.stroke();
+  draw() {
+    let pos = this.pos;
+    animation.draw_ball(pos);
   }
 };
 
@@ -101,54 +179,31 @@ class Player {
   }
 
   draw() {
-    this.ctx.save();
-    var r = 10;
-    this.ctx.translate(this.pos.x, this.pos.y);
-    this.ctx.rotate(this.angle);
-    //feets
-    this.ctx.beginPath();
-    this.ctx.arc(0 + 3*r/4, 0 - 2*r/3, r/2, 0, 2 * Math.PI);
-    this.ctx.arc(0 + 3*r/4, 0 + 2*r/3, r/2, 0, 2 * Math.PI);
-    this.ctx.fillStyle = "#000000";
-    this.ctx.fill();
-    if (this.has_ball) {
-      // ball
-      this.ctx.beginPath();
-      this.ctx.arc(0 + 3*r/2, 0 - r, r/2, 0, 2 * Math.PI);
-      this.ctx.fillStyle = "#ffffff";
-      this.ctx.fill();
-      this.ctx.stroke();
-    }
-    // body
-    this.ctx.beginPath();
-    this.ctx.ellipse(0, 0, r, r + r/2, 0, 0, 2 * Math.PI)
-    this.ctx.fillStyle = this.color;
-    this.ctx.fill();
-    this.ctx.stroke();
-    // head
-    this.ctx.beginPath();
-    this.ctx.arc(0, 0, r/2, 0, 2 * Math.PI);
-    this.ctx.fillStyle = "#000000";
-    this.ctx.fill();
-    this.ctx.restore();
+    let pos = this.pos;
+    let angle = this.angle;
+    let has_ball = this.has_ball;
+    let color = this.color;
+    animation.draw_player(pos, angle, color, has_ball);
   }
 
   go(len) {
-    let end_pos = calc_end_pos(this.pos, len, this.angle);
-    draw_line(this.pos, end_pos, [5, 10]);
+    let pos = this.pos;
+    let end_pos = calc_end_pos(pos, len, this.angle);
+    animation.draw_line(pos, end_pos, [5, 10]);
     this.pos = end_pos;
-    this.draw();
+    // this.draw();
   }
 
   rotate(angle_degrees) {
     this.angle += (angle_degrees * (Math.PI / 180));
-    this.draw();
+    // this.draw();
   }
 
   shoot(power) {
     if (this.has_ball) {
-      let end_pos = calc_end_pos(this.pos, power * 100, this.angle);
-      draw_line(this.pos, end_pos, [3, 3]);
+      let pos = this.pos;
+      let end_pos = calc_end_pos(pos, power * 100, this.angle);
+      animation.draw_line(pos, end_pos, [3, 3]);
       ball.pos = end_pos;
       ball.draw();
       this.has_ball = false;
@@ -168,12 +223,15 @@ var editor = null;
 function run() {
   var canvas = document.getElementById("myCanvas");
   var ctx = canvas.getContext("2d");
+  animation = new Animation(ctx);
   ball = new Ball(ctx, new Point(0, 0));
   player = new Player(ctx, player_start_pos, player_start_angle, "red", ball, true);
   goal = new Goal(ctx, new Point(750, 245), 40, 80);
   var imageObj = new Image();
   imageObj.src = "boisko.jpg";
-  imageObj.onload = function(){
+  new Promise((resolve, reject) => {
+    imageObj.onload = resolve;
+  }).then(() => {
     ctx.drawImage(imageObj, 0, 0);
     ctx.lineWidth = 1;
     ctx.fillStyle = "#FFFFFF";
@@ -192,16 +250,15 @@ function run() {
     player.draw();
     is_goal = goal.is_ball_in(ball.pos);
     if (is_goal) {
-      ctx.fillStyle = "yellow";
-      ctx.font = "48pt sans-serif";
-      ctx.fillText("GOAL!!!", 300, 300);
+      animation.draw_message("GOAL!!!", new Point(300, 300), "yellow", "48pt sans-serif");
     }
     var isGoal = document.getElementById("isGoal");
     isGoal.textContent = (is_goal ? "Goal!" : "No goal");
     var button_done = document.getElementById("buttonDone");
     button_done.disabled = !is_goal;
-  };
+  });
 };
+
 
 function go(len) {
   if (len > 200) {
@@ -247,8 +304,9 @@ go(100);
 rotate(45);
 go(100);
 rotate(45);
-go(100);
-shoot(3);
+go(150);
+rotate(-90);
+shoot(5);
 </textarea></p>
 <table width="80%" border="0"><tr><td>
 <p align="center" style="font-size: 20px; color: black;" id="isGoal"/></td><td>
